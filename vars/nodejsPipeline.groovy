7db6b212466a6fd7a8a0024e30fec01015d8c9fd
Brandon Awan <74030681+Brandonawan@users.noreply.github.com>
2023-08-10 18:47:46 +0100

    Update nodejsPipeline.groovy

--- vars/nodejsPipeline.groovy
+++ vars/nodejsPipeline.groovy
@@ -1,24 +1,8 @@
 // pipeline-library-demo/vars/nodejsPipeline.groovy
 def call(body) {
-    // Define the default values for stages
-    def stages = [:]
+    def myNodejsPipeline = new MyNodejsPipeline()
 
-    // Execute the provided closure to modify the stages
     body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = stages
+    body.delegate = myNodejsPipeline
     body()
-
-    // Define the Node.js pipeline stages
-    pipeline {
-        agent any
-        stages.each { stageName, stageBody ->
-            stage(stageName) {
-                steps {
-                    script {
-                        stageBody()
-                    }
-                }
-            }
-        }
-    }
 }
