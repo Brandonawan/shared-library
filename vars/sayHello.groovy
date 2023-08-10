dd783696f11314da225d386e1b3561d1d8fbd2c3
Brandon Awan <abibangbrandon855@gmail.com>
2023-08-10 18:28:28 +0100

    update

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,4 +1,34 @@
-#!/usr/bin/env groovy
-def call(String name = 'human') {
-  echo "Hello, ${name}."
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    pipeline {
+        agent any
+        stages {
+            stage('Build') {
+                steps {
+                    script {
+                        config.nodejsBuild()
+                    }
+                }
+            }
+            stage('Test') {
+                steps {
+                    script {
+                        config.nodejsTest()
+                    }
+                }
+            }
+            stage('Deploy') {
+                steps {
+                    script {
+                        config.nodejsDeploy()
+                    }
+                }
+            }
+        }
+    }
 }
+
