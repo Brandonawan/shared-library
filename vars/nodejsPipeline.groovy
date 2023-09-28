8206bc8074f4637cf46249f7c484be2fc40848a7
Brandon Awan <abibangbrandon855@gmail.com>
2023-09-28 12:51:59 +0100

    updates

--- vars/nodejsPipeline.groovy
+++
@@ -1,8 +0,0 @@
-// pipeline-library-demo/vars/nodejsPipeline.groovy
-def call(body) {
-    def myNodejsPipeline = new MyNodejsPipeline()
-
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = myNodejsPipeline
-    body()
-}
