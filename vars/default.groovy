69d347469bef6e7692574cea5252c87a94480987
Brandon Awan <abibangbrandon855@gmail.com>
2023-09-28 16:07:17 +0100

    updates

--- vars/default.groovy
+++ vars/default.groovy
@@ -1,35 +1,15 @@
 #!/usr/bin/env groovy
 
+// vars/default.groovy
+
 def call() {
     pipeline {
         agent any
 
         stages {
-            stage('Read YAML') {
+            stage('Default Stage') {
                 steps {
-                    script {
-                        // Define yamlData within the script block
-                        def yamlData = readYaml file: 'pipeline-config.yml'
-                        
-                        // Access data from the YAML file
-                        def name = yamlData.name
-                        def age = yamlData.age
-                        def email = yamlData.email
-                        
-                        // Print the values for demonstration
-                        echo "Name: ${name}"
-                        echo "Age: ${age}"
-                        echo "Email: ${email}"
-                    }
-                }
-            }
-
-            stage('Run jenkin-build Script') {
-                steps {
-                    script {
-                        // Call the external script without passing arguments
-                        sh './jenkin-build'
-                    }
+                    echo "This is the default stage."
                 }
             }
         }
