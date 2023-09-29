f8754b7c6e32f6dca8cbe497d1a5298c188f94b3
Brandon Awan <abibangbrandon855@gmail.com>
2023-09-29 22:17:51 +0100

    updates

--- vars/runMyPipeline.groovy
+++ vars/runMyPipeline.groovy
@@ -1,94 +1,96 @@
 #!/usr/bin/env groovy
 
+// def call() {
+//     pipeline {
+//         agent any
+
+//         stages {
+//             stage('Read Config YAML') {
+//                 steps {
+//                     script {
+//                         // Define yamlConfig within the script block
+//                         def yamlConfig = readYaml file: 'pipeline-config.yml'
+
+//                         // Access the dockerImage value from the YAML file
+//                         def dockerImage = yamlConfig.dockerImage
+
+//                         // Print the dockerImage value for demonstration
+//                         echo "Docker Image: ${dockerImage}"
+//                     }
+//                 }
+//             }
+
+//             stage('Run Inside Docker Image') {
+//                 steps {
+//                     script {
+//                         // Use the Docker image from the YAML configuration
+//                         agent {
+//                             docker {
+//                                 image dockerImage
+//                                 args '--user=root'
+//                             }
+//                         }
+
+//                         // The rest of your pipeline steps remain the same
+//                         sh 'apt-get update'
+//                         sh 'apt-get install -y python3-venv python3-pip' // Install Python virtualenv and pip
+//                         sh 'python3 -m venv venv' // Create a virtual environment
+//                         sh '. venv/bin/activate' // Activate the virtual environment using dot command
+
+//                         // Install dependencies (if you have a requirements.txt file)
+//                         sh 'pip install -r requirements.txt'
+
+//                         // Run tests (adjust the command accordingly)
+//                         sh 'pytest --junitxml=pytest-results.xml'
+
+//                         // Deactivate the virtual environment using the deactivate function
+//                         sh 'deactivate || true' // Use '|| true' to ignore errors if deactivate fails
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
 def call() {
-    pipeline {
-        agent any
+        pipeline {
+        agent {
+            docker {
+                // Pull the Ubuntu image and run as root user
+                image 'ubuntu:latest'
+                args '--user=root'
+            }
+        }
 
         stages {
-            stage('Read Config YAML') {
+            stage('Checkout') {
                 steps {
-                    script {
-                        // Define yamlConfig within the script block
-                        def yamlConfig = readYaml file: 'pipeline-config.yml'
-
-                        // Access the dockerImage value from the YAML file
-                        def dockerImage = yamlConfig.dockerImage
-
-                        // Print the dockerImage value for demonstration
-                        echo "Docker Image: ${dockerImage}"
-                    }
+                    checkout scm
                 }
             }
 
-            stage('Run Inside Docker Image') {
+            stage('Run Inside Ubuntu Docker Image') {
                 steps {
-                    script {
-                        // Use the Docker image from the YAML configuration
-                        agent {
-                            docker {
-                                image dockerImage
-                                args '--user=root'
-                            }
-                        }
+                    sh 'apt-get update'
+                    sh 'apt-get install -y python3-venv python3-pip' // Install Python virtualenv and pip
+                    sh 'python3 -m venv venv' // Create a virtual environment
+                    sh '. venv/bin/activate' // Activate the virtual environment using dot command
 
-                        // The rest of your pipeline steps remain the same
-                        sh 'apt-get update'
-                        sh 'apt-get install -y python3-venv python3-pip' // Install Python virtualenv and pip
-                        sh 'python3 -m venv venv' // Create a virtual environment
-                        sh '. venv/bin/activate' // Activate the virtual environment using dot command
+                    // Install dependencies (if you have a requirements.txt file)
+                    sh 'pip install -r requirements.txt'
 
-                        // Install dependencies (if you have a requirements.txt file)
-                        sh 'pip install -r requirements.txt'
+                    // Run tests (adjust the command accordingly)
+                    sh 'pytest --junitxml=pytest-results.xml'
 
-                        // Run tests (adjust the command accordingly)
-                        sh 'pytest --junitxml=pytest-results.xml'
-
-                        // Deactivate the virtual environment using the deactivate function
-                        sh 'deactivate || true' // Use '|| true' to ignore errors if deactivate fails
-                    }
+                    // Deactivate the virtual environment using the deactivate function
+                    sh 'deactivate || true' // Use '|| true' to ignore errors if deactivate fails
                 }
             }
         }
     }
 }
 
-// def call() {
-//         pipeline {
-//         agent {
-//             docker {
-//                 // Pull the Ubuntu image and run as root user
-//                 image 'ubuntu:latest'
-//                 args '--user=root'
-//             }
-//         }
 
-//         stages {
-//             stage('Checkout') {
-//                 steps {
-//                     checkout scm
-//                 }
-//             }
-
-//             stage('Run Inside Ubuntu Docker Image') {
-//                 steps {
-//                     sh 'apt-get update'
-//                     sh 'apt-get install -y python3-venv python3-pip' // Install Python virtualenv and pip
-//                     sh 'python3 -m venv venv' // Create a virtual environment
-//                     sh '. venv/bin/activate' // Activate the virtual environment using dot command
-
-//                     // Install dependencies (if you have a requirements.txt file)
-//                     sh 'pip install -r requirements.txt'
-
-//                     // Run tests (adjust the command accordingly)
-//                     sh 'pytest --junitxml=pytest-results.xml'
-
-//                     // Deactivate the virtual environment using the deactivate function
-//                     sh 'deactivate || true' // Use '|| true' to ignore errors if deactivate fails
-//                 }
-//             }
-//         }
-//     }
-// }
 // def call() {
 //     pipeline {
 //         agent any
