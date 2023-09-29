8a66763402359413295f112452132239d684a03f
Brandon Awan <abibangbrandon855@gmail.com>
2023-09-29 19:47:46 +0100

    updates 2023-09-29 19:47:46

--- vars/runMyPipeline.groovy
+++ vars/runMyPipeline.groovy
@@ -1,42 +1,51 @@
 #!/usr/bin/env groovy
 
 def call() {
-        pipeline {
-        agent {
-            docker {
-                // Pull the Ubuntu image and run as root user
-                image 'ubuntu:latest'
-                args '--user=root'
-            }
-        }
+    pipeline {
+        agent any
 
         stages {
-            stage('Checkout') {
+            stage('Read Config YAML') {
                 steps {
-                    checkout scm
+                    script {
+                        // Define yamlConfig within the script block
+                        def yamlConfig = readYaml file: 'pipeline-config.yml'
+
+                        // Access the dockerImage value from the YAML file
+                        def dockerImage = yamlConfig.dockerImage
+
+                        // Print the dockerImage value for demonstration
+                        echo "Docker Image: ${dockerImage}"
+                    }
                 }
             }
 
-            stage('Run Inside Ubuntu Docker Image') {
+            stage('Run Inside Docker Image') {
                 steps {
-                    sh 'apt-get update'
-                    sh 'apt-get install -y python3-venv python3-pip' // Install Python virtualenv and pip
-                    sh 'python3 -m venv venv' // Create a virtual environment
-                    sh '. venv/bin/activate' // Activate the virtual environment using dot command
+                    script {
+                        // Use the Docker image from the YAML configuration
+                        agent {
+                            docker {
+                                image dockerImage
+                                args '--user=root'
+                            }
+                        }
 
-                    // Install dependencies (if you have a requirements.txt file)
-                    sh 'pip install -r requirements.txt'
-
-                    // Run tests (adjust the command accordingly)
-                    sh 'pytest --junitxml=pytest-results.xml'
-
-                    // Deactivate the virtual environment using the deactivate function
-                    sh 'deactivate || true' // Use '|| true' to ignore errors if deactivate fails
+                        // The rest of your pipeline steps remain the same
+                        sh 'apt-get update'
+                        sh 'apt-get install -y python3-venv python3-pip'
+                        sh 'python3 -m venv venv'
+                        sh '. venv/bin/activate'
+                        sh 'pip install -r requirements.txt'
+                        sh 'pytest --junitxml=pytest-results.xml'
+                        sh 'deactivate || true'
+                    }
                 }
             }
         }
     }
 }
+
 // def call() {
 //     pipeline {
 //         agent any
