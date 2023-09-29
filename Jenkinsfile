7c2ab7d4bf89122bbbeaf1f99ff8f600069cda82
Brandon Awan <abibangbrandon855@gmail.com>
2023-09-29 18:33:59 +0100

    updates 2023-09-29 18:33:59

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,42 +1,42 @@
-// @Library('pipeline-library-demo')_
-// runMyPipeline()
+@Library('pipeline-library-demo')_
+runMyPipeline()
 
 
-pipeline {
-    agent {
-        docker {
-            // Pull the Ubuntu image and run as root user
-            image 'ubuntu:latest'
-            args '--user=root'
-        }
-    }
+// pipeline {
+//     agent {
+//         docker {
+//             // Pull the Ubuntu image and run as root user
+//             image 'ubuntu:latest'
+//             args '--user=root'
+//         }
+//     }
 
-    stages {
-        stage('Checkout') {
-            steps {
-                checkout scm
-            }
-        }
+//     stages {
+//         stage('Checkout') {
+//             steps {
+//                 checkout scm
+//             }
+//         }
 
-        stage('Run Inside Ubuntu Docker Image') {
-            steps {
-                sh 'apt-get update'
-                sh 'apt-get install -y python3-venv python3-pip' // Install Python virtualenv and pip
-                sh 'python3 -m venv venv' // Create a virtual environment
-                sh '. venv/bin/activate' // Activate the virtual environment using dot command
+//         stage('Run Inside Ubuntu Docker Image') {
+//             steps {
+//                 sh 'apt-get update'
+//                 sh 'apt-get install -y python3-venv python3-pip' // Install Python virtualenv and pip
+//                 sh 'python3 -m venv venv' // Create a virtual environment
+//                 sh '. venv/bin/activate' // Activate the virtual environment using dot command
 
-                // Install dependencies (if you have a requirements.txt file)
-                sh 'pip install -r requirements.txt'
+//                 // Install dependencies (if you have a requirements.txt file)
+//                 sh 'pip install -r requirements.txt'
 
-                // Run tests (adjust the command accordingly)
-                sh 'pytest --junitxml=pytest-results.xml'
+//                 // Run tests (adjust the command accordingly)
+//                 sh 'pytest --junitxml=pytest-results.xml'
 
-                // Deactivate the virtual environment using the deactivate function
-                sh 'deactivate || true' // Use '|| true' to ignore errors if deactivate fails
-            }
-        }
-    }
-}
+//                 // Deactivate the virtual environment using the deactivate function
+//                 sh 'deactivate || true' // Use '|| true' to ignore errors if deactivate fails
+//             }
+//         }
+//     }
+// }
 
 
 
--- vars/runMyPipeline.groovy
+++ vars/runMyPipeline.groovy
@@ -1,41 +1,77 @@
 #!/usr/bin/env groovy
 
-
 def call() {
-    pipeline {
-        agent any
+        pipeline {
+        agent {
+            docker {
+                // Pull the Ubuntu image and run as root user
+                image 'ubuntu:latest'
+                args '--user=root'
+            }
+        }
 
         stages {
-            stage('Read YAML') {
+            stage('Checkout') {
                 steps {
-                    script {
-                        // Define yamlData within the script block
-                        def yamlData = readYaml file: 'pipeline-config.yml'
-                        
-                        // Access data from the YAML file
-                        def name = yamlData.name
-                        def age = yamlData.age
-                        def email = yamlData.email
-                        
-                        // Print the values for demonstration
-                        echo "Name: ${name}"
-                        echo "Age: ${age}"
-                        echo "Email: ${email}"
-                    }
+                    checkout scm
                 }
             }
 
-            stage('Run jenkin-build Script') {
+            stage('Run Inside Ubuntu Docker Image') {
                 steps {
-                    script {
-                        // Call the external script without passing arguments
-                        sh './jenkin-build'
-                    }
+                    sh 'apt-get update'
+                    sh 'apt-get install -y python3-venv python3-pip' // Install Python virtualenv and pip
+                    sh 'python3 -m venv venv' // Create a virtual environment
+                    sh '. venv/bin/activate' // Activate the virtual environment using dot command
+
+                    // Install dependencies (if you have a requirements.txt file)
+                    sh 'pip install -r requirements.txt'
+
+                    // Run tests (adjust the command accordingly)
+                    sh 'pytest --junitxml=pytest-results.xml'
+
+                    // Deactivate the virtual environment using the deactivate function
+                    sh 'deactivate || true' // Use '|| true' to ignore errors if deactivate fails
                 }
             }
         }
     }
 }
+// def call() {
+//     pipeline {
+//         agent any
+
+//         stages {
+//             stage('Read YAML') {
+//                 steps {
+//                     script {
+//                         // Define yamlData within the script block
+//                         def yamlData = readYaml file: 'pipeline-config.yml'
+                        
+//                         // Access data from the YAML file
+//                         def name = yamlData.name
+//                         def age = yamlData.age
+//                         def email = yamlData.email
+                        
+//                         // Print the values for demonstration
+//                         echo "Name: ${name}"
+//                         echo "Age: ${age}"
+//                         echo "Email: ${email}"
+//                     }
+//                 }
+//             }
+
+//             stage('Run jenkin-build Script') {
+//                 steps {
+//                     script {
+//                         // Call the external script without passing arguments
+//                         sh './jenkin-build'
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
 
 
 
