8aec37156b95a59b1740744b0eb0f602c852dce2
Brandon Awan <abibangbrandon855@gmail.com>
2023-09-29 17:32:53 +0100

    updates 2023-09-29 17:32:53

---
+++ push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if there are any changes to commit
+if [[ -n $(git status -s) ]]; then
+    # Perform 'git add .' to stage all changes
+    git add .
+
+    # Create a timestamp for the commit message
+    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+
+    # Commit changes with a timestamped message
+    git commit -m "updates ${timestamp}"
+
+    # Push changes to the remote repository
+    git push
+
+    echo "Changes committed and pushed."
+else
+    echo "No changes to commit."
+fi
