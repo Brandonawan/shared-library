d9cd8b2f69ba9c9d9b1971e7ab1bf3df8b5d7d95
Brandon Awan <abibangbrandon855@gmail.com>
2023-09-29 20:07:09 +0100

    updates 2023-09-29 20:07:09

--- vars/runMyPipeline.groovy
+++ vars/runMyPipeline.groovy
@@ -31,9 +31,9 @@ def call() {
                             }
                         }
 
-                        // The rest of your pipeline steps remain the same
-                        sh 'apt-get update'
-                        sh 'apt-get install -y python3-venv python3-pip'
+                        // Update package lists and install packages with sudo
+                        sh 'sudo apt-get update'
+                        sh 'sudo apt-get install -y python3-venv python3-pip'
                         sh 'python3 -m venv venv'
                         sh '. venv/bin/activate'
                         sh 'pip install -r requirements.txt'
@@ -46,6 +46,44 @@ def call() {
     }
 }
 
+
+// def call() {
+//         pipeline {
+//         agent {
+//             docker {
+//                 // Pull the Ubuntu image and run as root user
+//                 image 'ubuntu:latest'
+//                 args '--user=root'
+//             }
+//         }
+
+//         stages {
+//             stage('Checkout') {
+//                 steps {
+//                     checkout scm
+//                 }
+//             }
+
+//             stage('Run Inside Ubuntu Docker Image') {
+//                 steps {
+//                     sh 'apt-get update'
+//                     sh 'apt-get install -y python3-venv python3-pip' // Install Python virtualenv and pip
+//                     sh 'python3 -m venv venv' // Create a virtual environment
+//                     sh '. venv/bin/activate' // Activate the virtual environment using dot command
+
+//                     // Install dependencies (if you have a requirements.txt file)
+//                     sh 'pip install -r requirements.txt'
+
+//                     // Run tests (adjust the command accordingly)
+//                     sh 'pytest --junitxml=pytest-results.xml'
+
+//                     // Deactivate the virtual environment using the deactivate function
+//                     sh 'deactivate || true' // Use '|| true' to ignore errors if deactivate fails
+//                 }
+//             }
+//         }
+//     }
+// }
 // def call() {
 //     pipeline {
 //         agent any
