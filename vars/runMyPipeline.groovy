104f42f81b43bf85bdc93103b41d0ccd99ac96b6
Brandon Awan <abibangbrandon855@gmail.com>
2023-10-02 17:40:00 +0100

    updates 2023-10-02 17:40:00

--- vars/runMyPipeline.groovy
+++ vars/runMyPipeline.groovy
@@ -14,10 +14,10 @@ def call() {
 
                         // Validate pipeline-config.yml
                         dockerConfig = readYaml file: 'pipeline-config.yml' // Assign to the higher scope variable
-                        // validateDockerConfig('${WORKSPACE}/dockerConfig')
+                        validateDockerConfig("${WORKSPACE}/${env.JOB_NAME}/dockerConfig")
 
                         // Check if jenkin-build script exists and is executable
-                        // validateExecutableScript('${WORKSPACE}/jenkin-build')
+                        validateExecutableScript("${WORKSPACE}/${env.JOB_NAME}/jenkin-build")
 
                         // Set default values if not provided
                         dockerConfig.dockerImage = dockerConfig.dockerImage ?: 'maven:3-alpine'
