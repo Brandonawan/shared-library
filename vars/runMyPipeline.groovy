1c27f8c06ebc7d04030ccae26e6f368c85ca2d69
Brandon Awan <abibangbrandon855@gmail.com>
2023-10-02 16:39:58 +0100

    updates 2023-10-02 16:39:58

--- vars/runMyPipeline.groovy
+++ vars/runMyPipeline.groovy
@@ -1,91 +1,89 @@
 #!/usr/bin/env groovy
 
 
-def call(){
-    def dockerConfig // Declare dockerConfig at a higher scope
+def dockerConfig // Declare dockerConfig at a higher scope
 
-    def call() {
-        pipeline {
-            agent any
-            stages {
-                stage('Validation and Setup') {
-                    steps {
-                        script {
-                            sh 'pwd'  // Print the current working directory (CWD)
-                            sh 'ls -la'  // List all files and their permissions in the current directory
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage('Validation and Setup') {
+                steps {
+                    script {
+                        sh 'pwd'  // Print the current working directory (CWD)
+                        sh 'ls -la'  // List all files and their permissions in the current directory
 
-                            // Validate pipeline-config.yml
-                            dockerConfig = readYaml file: 'pipeline-config.yml' // Assign to the higher scope variable
+                        // Validate pipeline-config.yml
+                        dockerConfig = readYaml file: 'pipeline-config.yml' // Assign to the higher scope variable
 
-                            // Check if buildscript file exists
-                            if (!fileExists('jenkin-build')) {
-                                error('buildscript file is missing.')
-                            }
-
-                            // Check if YAML file exists
-                            if (!fileExists('pipeline-config.yml')) {
-                                error('pipeline-config.yml file is missing.')
-                            }
-
-                            // Check if buildscript is executable
-                            if (!isScriptExecutable('jenkin-build')) {
-                                error('buildscript is not executable. Make sure to run `chmod +x buildscript` before committing.')
-                            }
-
-                            // Set default values if not provided
-                            dockerConfig.dockerImage = dockerConfig.dockerImage ?: 'maven:3-alpine'
+                        // Check if buildscript file exists
+                        if (!fileExists('buildscript')) {
+                            error('buildscript file is missing.')
                         }
-                    }
-                }
 
-                stage('Checkout') {
-                    steps {
-                        checkout scm
-                    }
-                }
-
-                stage('Run Inside Docker Image') {
-                    agent {
-                        docker {
-                            image "${dockerConfig.dockerImage}"
-                            args '--user=root'
+                        // Check if YAML file exists
+                        if (!fileExists('pipeline-config.yml')) {
+                            error('pipeline-config.yml file is missing.')
                         }
-                    }
-                    steps {
-                        sh 'apt-get update'
-                        sh 'apt-get install -y python3-venv python3-pip' // Install Python virtualenv and pip
-                        sh 'python3 -m venv venv' // Create a virtual environment
-                        sh '. venv/bin/activate' // Activate the virtual environment using the dot command
 
-                        // Install dependencies (if you have a requirements.txt file)
-                        sh 'pip install -r requirements.txt'
+                        // Check if buildscript is executable
+                        if (!isScriptExecutable('buildscript')) {
+                            error('buildscript is not executable. Make sure to run `chmod +x buildscript` before committing.')
+                        }
 
-                        // Run tests (adjust the command accordingly)
-                        sh 'pytest --junitxml=pytest-results.xml'
-
-                        // Deactivate the virtual environment using the deactivate function
-                        sh 'deactivate || true' // Use '|| true' to ignore errors if deactivate fails
+                        // Set default values if not provided
+                        dockerConfig.dockerImage = dockerConfig.dockerImage ?: 'maven:3-alpine'
                     }
                 }
+            }
 
-                stage('Deliver') {
-                    steps {
-                        sh "${WORKSPACE}/buildscript"
+            stage('Checkout') {
+                steps {
+                    checkout scm
+                }
+            }
+
+            stage('Run Inside Docker Image') {
+                agent {
+                    docker {
+                        image "${dockerConfig.dockerImage}"
+                        args '--user=root'
                     }
+                }
+                steps {
+                    sh 'apt-get update'
+                    sh 'apt-get install -y python3-venv python3-pip' // Install Python virtualenv and pip
+                    sh 'python3 -m venv venv' // Create a virtual environment
+                    sh '. venv/bin/activate' // Activate the virtual environment using the dot command
+
+                    // Install dependencies (if you have a requirements.txt file)
+                    sh 'pip install -r requirements.txt'
+
+                    // Run tests (adjust the command accordingly)
+                    sh 'pytest --junitxml=pytest-results.xml'
+
+                    // Deactivate the virtual environment using the deactivate function
+                    sh 'deactivate || true' // Use '|| true' to ignore errors if deactivate fails
+                }
+            }
+
+            stage('Deliver') {
+                steps {
+                    sh "${WORKSPACE}/buildscript"
                 }
             }
         }
     }
-
-    def fileExists(fileName) {
-        return new File(fileName).exists()
-    }
-
-    def isScriptExecutable(scriptName) {
-        return new File(scriptName).canExecute()
-    }
-
 }
+
+def fileExists(fileName) {
+    return new File(fileName).exists()
+}
+
+def isScriptExecutable(scriptName) {
+    return new File(scriptName).canExecute()
+}
+
 
 // def dockerConfig // Declare dockerConfig at a higher scope
 
