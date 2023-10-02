c5d8a355ba316accee81f3bdd79e4c18c2aba4a1
Brandon Awan <abibangbrandon855@gmail.com>
2023-10-02 14:06:11 +0100

    updates 2023-10-02 14:06:11

--- vars/runMyPipeline.groovy
+++ vars/runMyPipeline.groovy
@@ -1,75 +1,48 @@
 #!/usr/bin/env groovy
 
+
 def call() {
-  pipeline {
-    agent any
-
-    stages {
-      stage('Run Inside Ubuntu Docker Image') {
-        steps {
-          script {
-            // Pull the Ubuntu image and run as the apt user, mounting the /var/lib/apt/lists directory
-            docker.image('ubuntu:latest').withRun('--user=root -v /var/lib/apt/lists:/var/lib/apt/lists') { c ->
-              // Inside the Docker container
-
-              // Update and install necessary packages
-              sh 'apt-get update'
-              sh 'apt-get install -y python3-venv python3-pip'
-
-              // Create and activate a virtual environment
-              sh 'python3 -m venv venv'
-              sh '. venv/bin/activate'
-
-              // If you have a requirements.txt file, install dependencies
-              sh 'pip install -r requirements.txt'
-
-              // Run tests (adjust the command accordingly)
-              sh 'pytest --junitxml=pytest-results.xml'
+        pipeline {
+        agent {
+            docker {
+                // Pull the Ubuntu image and run as root user
+                image 'ubuntu:latest'
+                args '--user=root'
             }
-          }
         }
-      }
+
+        stages {
+            stage('Checkout') {
+                steps {
+                    checkout scm
+                }
+            }
+
+            stage('Run Inside Ubuntu Docker Image') {
+                steps {
+                    sh 'apt-get update'
+                    sh 'apt-get install -y python3-venv python3-pip' // Install Python virtualenv and pip
+                    sh 'python3 -m venv venv' // Create a virtual environment
+                    sh '. venv/bin/activate' // Activate the virtual environment using dot command
+
+                    // Install dependencies (if you have a requirements.txt file)
+                    sh 'pip install -r requirements.txt'
+
+                    // Run tests (adjust the command accordingly)
+                    sh 'pytest --junitxml=pytest-results.xml'
+
+                    // Deactivate the virtual environment using the deactivate function
+                    sh 'deactivate || true' // Use '|| true' to ignore errors if deactivate fails
+                }
+            }
+            stage('Deliver') {
+            steps {
+                sh './jenkin-build'
+            }
+        }
+        }
     }
-  }
 }
-
-// def call() {
-//         pipeline {
-//         agent {
-//             docker {
-//                 // Pull the Ubuntu image and run as root user
-//                 image 'ubuntu:latest'
-//                 args '--user=root'
-//             }
-//         }
-
-//         stages {
-//             stage('Checkout') {
-//                 steps {
-//                     checkout scm
-//                 }
-//             }
-
-//             stage('Run Inside Ubuntu Docker Image') {
-//                 steps {
-//                     sh 'apt-get update'
-//                     sh 'apt-get install -y python3-venv python3-pip' // Install Python virtualenv and pip
-//                     sh 'python3 -m venv venv' // Create a virtual environment
-//                     sh '. venv/bin/activate' // Activate the virtual environment using dot command
-
-//                     // Install dependencies (if you have a requirements.txt file)
-//                     sh 'pip install -r requirements.txt'
-
-//                     // Run tests (adjust the command accordingly)
-//                     sh 'pytest --junitxml=pytest-results.xml'
-
-//                     // Deactivate the virtual environment using the deactivate function
-//                     sh 'deactivate || true' // Use '|| true' to ignore errors if deactivate fails
-//                 }
-//             }
-//         }
-//     }
-// }
 
 
 // def call() {
