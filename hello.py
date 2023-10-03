4c628c6b6c07b01c02c756aa36df165285a9077a
Brandon Awan <abibangbrandon855@gmail.com>
2023-10-03 17:33:35 +0100

    updates 2023-10-03 17:33:35

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
+# #!/usr/bin/env python3
+
+# def greet(name):
+#     return f"Hello, {name}!"
+
+# greet("World")
+
 #!/usr/bin/env python3
 
 def greet(name):
-    return f"Hello, {name}!"
+    message = f"Hello, {name}!"
+    print(message)  # Print the greeting message
 
 greet("World")
