da3474d1b925e1672fcc1e7b3d9a3df8c4c17922
Brandon Awan <abibangbrandon855@gmail.com>
2023-10-03 09:26:05 +0100

    updates 2023-10-03 09:26:05

--- vars/runMyPipeline.groovy
+++ vars/runMyPipeline.groovy
@@ -1,4 +1,5 @@
 #!/usr/bin/env groovy
+#!/usr/bin/env groovy
 
 // Define a default Docker image name
 def defaultDockerImage = 'debian:latest'
@@ -99,6 +100,110 @@ def checkIfJenkinBuildIsExecutable() {
         }
     }
 }
+
+// // Define a function to check files and run the pipeline
+// def call() {
+//     pipeline {
+//         agent any
+//         options {
+//             ansiColor('xterm')
+//             timestamps()
+//         }
+//         stages {
+//             stage('Checkout') {
+//                 steps {
+//                     checkout scm
+//                 }
+//             }
+
+//             // stage('Create Non-Executable File') {
+//             //     steps {
+//             //         script {
+//             //             writeFile file: 'jenkin-build', text: 'This is a non-executable file content'
+//             //         }
+//             //     }
+//             // }
+
+//             stage('Check Files') {
+//                 steps {
+//                     script {
+//                         checkFileExists('jenkin-build')
+//                         checkFileExists('pipeline-config.yml')
+
+//                         // Check if jenkin-build is executable
+//                         checkIfJenkinBuildIsExecutable()
+//                     }
+//                 }
+//             }
+
+//             stage('Read Docker Image Name') {
+//                 steps {
+//                     script {
+//                         def dockerConfig = readYaml file: 'pipeline-config.yml'
+//                         def dockerImage = dockerConfig.dockerImage
+
+//                         // Set the Docker image name as an environment variable
+//                         env.DOCKER_IMAGE = dockerImage
+//                     }
+//                 }
+//             }
+
+//             stage('Run Inside Docker Image') {
+//                 agent {
+//                     docker {
+//                         image "${env.DOCKER_IMAGE}"
+//                         args '--user=root'
+//                     }
+//                 }
+//                 steps {
+//                     sh 'apt-get update'
+//                     sh 'apt-get install -y python3-venv python3-pip' // Install Python virtualenv and pip
+//                     sh 'python3 -m venv venv' // Create a virtual environment
+//                     sh '. venv/bin/activate' // Activate the virtual environment using the dot command
+
+//                     // Install dependencies (if you have a requirements.txt file)
+//                     sh 'pip install -r requirements.txt'
+
+//                     // Run tests (adjust the command accordingly)
+//                     sh 'pytest --junitxml=pytest-results.xml'
+
+//                     // Deactivate the virtual environment using the deactivate function
+//                     sh 'deactivate || true' // Use '|| true' to ignore errors if deactivate fails
+//                 }
+//             }
+
+//             stage('Deliver') {
+//                 steps {
+//                     sh ''' #!/bin/bash
+//                     ./jenkin-build
+//                     '''
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// // Define a function to check if a file exists
+// def checkFileExists(fileName) {
+//     def fileExists = fileExists(fileName)
+//     if (!fileExists) {
+//         error "File '${fileName}' not found in the repository."
+//     }
+// }
+
+// // Define a function to check if jenkin-build is executable
+// def checkIfJenkinBuildIsExecutable() {
+//     script {
+//         if (!fileExists('jenkin-build')) {
+//             error "The 'jenkin-build' file is not found in the repository."
+//         }
+
+//         def isExecutable = sh(script: "test -x jenkin-build", returnStatus: true)
+//         if (isExecutable != 0) {
+//             error "The 'jenkin-build' file is not executable."
+//         }
+//     }
+// }
 
 
 
@@ -203,7 +308,6 @@ def checkIfJenkinBuildIsExecutable() {
 //         }
 //     }
 // }
-
 
 
 
