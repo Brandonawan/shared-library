aaef4d9885c02c3dbab8b268f20494809be85dc2
Brandon Awan <abibangbrandon855@gmail.com>
2023-10-04 08:30:18 +0100

    updates 2023-10-04 08:30:18

--- vars/test.groovy
+++ vars/test.groovy
@@ -3,8 +3,8 @@
 // Define a function to check files and run the pipeline
 def call() {
 
-    def jenkinBuildPath = 'jenkin-build'
-    def pipelineConfigPath = 'config/pipeline-config.yml'
+    def jenkinBuildPath = 'scripts/jenkin-build'
+    def pipelineConfigPath = 'scripts/pipeline-config.yml'
 
     pipeline {
         agent any
@@ -22,7 +22,11 @@ def call() {
             stage('Check Files') {
                 steps {
                     script {
-                        checkFileExists(jenkinBuildPath)
+                        if (jenkinBuildPath.isEmpty()) {
+                            error "No build script is found. Please specify a valid file path."
+                        }
+
+                        checkFileExistsInternal(jenkinBuildPath)
                         checkFileExists(pipelineConfigPath)
 
                         // Check if jenkin-build is executable
@@ -35,7 +39,7 @@ def call() {
                 steps {
                     script {
                         // Set the default Docker image name
-                        def dockerImage = 'fedora:30'
+                        def dockerImage = 'ubuntu:latest'
 
                         // Try to read the Docker image name from the pipeline-config.yml file
                         try {
@@ -58,7 +62,7 @@ def call() {
                 agent {
                     docker {
                         image "${env.DOCKER_IMAGE}"
-                        args '--user=root'
+                        args '--user=root -v /mnt:/mnt'
                         reuseNode(true) // Always pull the image if not available locally
                     }
                 }
@@ -71,19 +75,11 @@ def call() {
                     // Install dependencies (if you have a requirements.txt file)
                     sh 'pip install -r requirements.txt'
 
-                    // Run tests (adjust the command accordingly)
-                    sh 'pytest --junitxml=pytest-results.xml'
+                    // Run the script (adjust the command accordingly)
+                    sh "./${jenkinBuildPath}"
 
                     // Deactivate the virtual environment using the deactivate function
                     sh 'deactivate || true' // Use '|| true' to ignore errors if deactivate fails
-                }
-            }
-
-            stage('Deliver') {
-                steps {
-                    sh ''' #!/bin/bash
-                    ./jenkin-build
-                    '''
                 }
             }
         }
@@ -104,134 +100,11 @@ def checkFileExists(fileName) {
 def checkIfJenkinBuildIsExecutable(fileName) {
     if (!fileExists(fileName)) {
         error "The '${fileName}' file is not found in the repository."
-    } else {
-        def isExecutable = sh(script: "test -x ${fileName}", returnStatus: true)
-        if (isExecutable != 0) {
-            // Attempt to make the file executable
-            sh "chmod +x ${fileName}"
-            
-            // Check again if it's executable now
-            isExecutable = sh(script: "test -x ${fileName}", returnStatus: true)
-            if (isExecutable == 0) {
-                echo "Made '${fileName}' executable."
-            } else {
-                error "Failed to make '${fileName}' executable."
-            }
-        }
+    }
+
+    def isExecutable = sh(script: "test -x ${fileName}", returnStatus: true)
+    if (isExecutable != 0) {
+        error "The '${fileName}' file is not executable."
     }
 }
-
-
-// // Define a function to check files and run the pipeline
-// def call() {
-//     pipeline {
-//         agent any
-//         options {
-//             ansiColor('xterm')
-//             timestamps()
-//         }
-//         stages {
-//             stage('Checkout') {
-//                 steps {
-//                     checkout scm
-//                 }
-//             }
-
-//             // stage('Create Non-Executable File') {
-//             //     steps {
-//             //         script {
-//             //             writeFile file: 'jenkin-build', text: 'This is a non-executable file content'
-//             //         }
-//             //     }
-//             // }
-
-//             stage('Check Files') {
-//                 steps {
-//                     script {
-//                         checkFileExists('jenkin-build')
-//                         checkFileExists('pipeline-config.yml')
-
-//                         // Check if jenkin-build is executable
-//                         checkIfJenkinBuildIsExecutable()
-//                     }
-//                 }
-//             }
-
-//             stage('Read Docker Image Name') {
-//                 steps {
-//                     script {
-//                         def dockerConfig = readYaml file: 'pipeline-config.yml'
-//                         def dockerImage = dockerConfig.dockerImage
-
-//                         // Set the Docker image name as an environment variable
-//                         env.DOCKER_IMAGE = dockerImage
-//                     }
-//                 }
-//             }
-
-//             stage('Run Inside Docker Image') {
-//                 agent {
-//                     docker {
-//                         image "${env.DOCKER_IMAGE}"
-//                         args '--user=root'
-//                     }
-//                 }
-//                 steps {
-//                     sh 'apt-get update'
-//                     sh 'apt-get install -y python3-venv python3-pip' // Install Python virtualenv and pip
-//                     sh 'python3 -m venv venv' // Create a virtual environment
-//                     sh '. venv/bin/activate' // Activate the virtual environment using the dot command
-
-//                     // Install dependencies (if you have a requirements.txt file)
-//                     sh 'pip install -r requirements.txt'
-
-//                     // Run tests (adjust the command accordingly)
-//                     sh 'pytest --junitxml=pytest-results.xml'
-
-//                     // Deactivate the virtual environment using the deactivate function
-//                     sh 'deactivate || true' // Use '|| true' to ignore errors if deactivate fails
-//                 }
-//             }
-
-//             stage('Deliver') {
-//                 steps {
-//                     sh ''' #!/bin/bash
-//                     ./jenkin-build
-//                     '''
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// // Define a function to check if a file exists
-// def checkFileExists(fileName) {
-//     def fileExists = fileExists(fileName)
-//     if (!fileExists) {
-//         error "File '${fileName}' not found in the repository."
-//     }
-// }
-
-// // Define a function to check if jenkin-build is executable
-// def checkIfJenkinBuildIsExecutable() {
-//     script {
-//         if (!fileExists('jenkin-build')) {
-//             error "The 'jenkin-build' file is not found in the repository."
-//         }
-
-//         def isExecutable = sh(script: "test -x jenkin-build", returnStatus: true)
-//         if (isExecutable != 0) {
-//             error "The 'jenkin-build' file is not executable."
-//         }
-//     }
-// }
-
-
-
-
-
-
-
-
-
 
