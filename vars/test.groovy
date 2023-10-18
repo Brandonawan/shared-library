14633b1dfa88e15180f6298d413cb8187b52f749
Brandon Awan <abibangbrandon855@gmail.com>
2023-10-18 17:16:50 +0100

    updates 2023-10-18 17:16:50

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,6 +1,5 @@
 #!/usr/bin/env groovy
 
-// Define a function to check files and run the pipeline
 def call() {
 
     def jenkinsBuildPath = 'jenkins/jenkin-build'
@@ -14,9 +13,57 @@ def call() {
             timestamps()
         }
         stages {
+            // stage('Checkout') {
+            //     steps {
+            //         checkout scm
+            //     }
             stage('Checkout') {
                 steps {
-                    checkout scm
+                    script {
+                        def pipelineConfig = readYaml file: pipelineConfigPath
+                        def checkoutStrategy = pipelineConfig.scmCheckoutStrategy
+
+                        if (checkoutStrategy) {
+                            checkoutStrategy.each { strategy ->
+                                switch (strategy.strategyName) {
+                                    case 'default':
+                                        checkout scm
+                                        break
+                                    case 'clean-before-checkout':
+                                        deleteDir()
+                                        checkout scm
+                                        break
+                                    case 'revert-before-update':
+                                        dir(strategy.svnDirectory) {
+                                            sh "svn revert -R ."
+                                            checkout([$class: 'SubversionSCM', locations: [[remote: strategy.svnRepoUrl]]])
+                                        }
+                                        break
+                                    case 'wipe-out-workspace':
+                                        deleteDir()
+                                        checkout scm
+                                        break
+                                    case 'sparse-checkout':
+                                        checkout([$class: 'GitSCM', branches: [[name: strategy.gitBranch]],
+                                                  userRemoteConfigs: [[url: strategy.gitRepoUrl]],
+                                                  extensions: [[$class: 'SparseCheckoutPaths', sparseCheckoutPaths: [[path: strategy.gitSparsePath]]]])
+                                        break
+                                    case 'lightweight-checkout':
+                                        checkout([$class: 'GitSCM', branches: [[name: strategy.gitBranch]],
+                                                  userRemoteConfigs: [[url: strategy.gitRepoUrl]],
+                                                  extensions: [[$class: 'CleanCheckout'], [$class: 'CloneOption', depth: 1]])
+                                        break
+                                    case 'custom-checkout':
+                                        sh "sh ${strategy.checkoutScriptName}"
+                                        break
+                                    default:
+                                        error "Invalid checkout strategy: ${strategy.strategyName}"
+                                }
+                            }
+                        } else {
+                            error "No scmCheckoutStrategy defined in the configuration."
+                        }
+                    }
                 }
             }
 
@@ -24,7 +71,7 @@ def call() {
                 steps {
                     script {
                         if (jenkinsBuildPath.isEmpty()) {
-                            error("Error: No build script is found. Please specify a valid file path. Refer to the documentation for guidance: [${confluenceDocLink}]")
+                            error "Error: No build script is found. Please specify a valid file path. Refer to the documentation for guidance: [${confluenceDocLink}]"
                         }
 
                         checkFileExistsInternal(jenkinsBuildPath)
@@ -39,17 +86,15 @@ def call() {
             stage('Read Docker Image Name') {
                 steps {
                     script {
-                        // Try to read the Docker image name from the pipeline-config.yml file
                         try {
                             def dockerConfig = readYaml file: pipelineConfigPath
                             if (dockerConfig && dockerConfig.dockerImage) {
-                                // Set the Docker image name as an environment variable
                                 env.DOCKER_IMAGE = dockerConfig.dockerImage
                             } else {
-                                error("Error: Docker image name not found in ${pipelineConfigPath}. Please specify a Docker image name in the configuration. Refer to the documentation for guidance: [${confluenceDocLink}]")
+                                error "Error: Docker image name not found in ${pipelineConfigPath}. Please specify a Docker image name in the configuration. Refer to the documentation for guidance: [${confluenceDocLink}]"
                             }
                         } catch (e) {
-                            error("Error: Could not read Docker image name from ${pipelineConfigPath}. Please check the configuration. Refer to the documentation for guidance: [${confluenceDocLink}]")
+                            error "Error: Could not read Docker image name from ${pipelineConfigPath}. Please check the configuration. Refer to the documentation for guidance: [${confluenceDocLink}]"
                         }
                     }
                 }
@@ -60,23 +105,20 @@ def call() {
                     docker {
                         image "${env.DOCKER_IMAGE}"
                         args '--user=root -v /mnt:/mnt'
-                        reuseNode(true) // Always pull the image if not available locally
+                        reuseNode(true)
                     }
                 }
                 steps {
                     sh 'apt-get update'
-                    sh 'apt-get install -y python3-venv python3-pip' // Install Python virtualenv and pip
-                    sh 'python3 -m venv venv' // Create a virtual environment
-                    sh '. venv/bin/activate' // Activate the virtual environment using the dot command
+                    sh 'apt-get install -y python3-venv python3-pip'
+                    sh 'python3 -m venv venv'
+                    sh '. venv/bin/activate'
 
-                    // Install dependencies (if you have a requirements.txt file)
                     sh 'pip install -r requirements.txt'
 
-                    // Run the script (adjust the command accordingly)
                     sh "./${jenkinsBuildPath}"
 
-                    // Deactivate the virtual environment using the deactivate function
-                    sh 'deactivate || true' // Use '|| true' to ignore errors if deactivate fails
+                    sh 'deactivate || true'
                 }
             }
 
@@ -94,7 +136,7 @@ def call() {
 def checkFileExistsInternal(fileName) {
     def fileExists = fileExists(fileName)
     if (!fileExists) {
-        error("Error: File '${fileName}' not found in the repository. Refer to the documentation for guidance: [${confluenceDocLink}]")
+        error "Error: File '${fileName}' not found in the repository. Refer to the documentation for guidance: [${confluenceDocLink}]"
     }
 }
 
@@ -104,11 +146,11 @@ def checkFileExists(fileName) {
 
 def checkIfJenkinsBuildIsExecutable(fileName) {
     if (!fileExists(fileName)) {
-        error("Error: The '${fileName}' file is not found in the repository. Refer to the documentation for guidance: [${confluenceDocLink}]")
+        error "Error: The '${fileName}' file is not found in the repository. Refer to the documentation for guidance: [${confluenceDocLink}]"
     }
 
     def isExecutable = sh(script: "test -x ${fileName}", returnStatus: true)
     if (isExecutable != 0) {
-        error("Error: The '${fileName}' file is not executable. Refer to the documentation for guidance: [${confluenceDocLink}]")
+        error "Error: The '${fileName}' file is not executable. Refer to the documentation for guidance: [${confluenceDocLink}]"
     }
 }
