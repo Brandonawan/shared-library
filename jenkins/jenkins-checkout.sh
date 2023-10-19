07edecf309d180d029b28eb2bffc7823f4ea9527
Brandon Awan <abibangbrandon855@gmail.com>
2023-10-19 06:26:39 +0100

    updates 2023-10-19 06:26:39

--- jenkins/jenkins-checkout.sh
+++ jenkins/jenkins-checkout.sh
@@ -1,8 +1,22 @@
+# #!/bin/bash
+
+# # Replace this with your custom checkout logic
+# echo "Running custom checkout script..."
+
+# git clone git@github.com:Brandonawan/shared-library.git
+# # Add any other custom checkout steps here
+
+
 #!/bin/bash
 
-# Replace this with your custom checkout logic
-echo "Running custom checkout script..."
+# Get the current workspace directory
+WORKSPACE=`pwd`
 
+# Change to the specified checkout directory
+cd "${WORKSPACE}/your-checkout-directory"
+
+# Perform the custom checkout operation
 git clone git@github.com:Brandonawan/shared-library.git
+
 # Add any other custom checkout steps here
 
