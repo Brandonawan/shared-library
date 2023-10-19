3080388c58c8204e49fdd839dc0a0a649f5f5e72
Brandon Awan <abibangbrandon855@gmail.com>
2023-10-19 17:56:31 +0100

    updates 2023-10-19 17:56:31

--- vars/test.groovy
+++ vars/test.groovy
@@ -59,14 +59,17 @@ def call() {
                     }
                 }
             }
-
             stage("git clone") {
                 steps {
                     echo "Starting 'git clone' stage"
-                    sh "git clone https://github.com/axumt/axumt-shared-library.git"
+
+                    // Replace 'YOUR_TOKEN' with your actual personal access token
+                    sh "git clone https://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@github.com/axumt/axumt-shared-library.git"
+
                     echo "git clone completed"
                 }
             }
+
             
             stage('Check Files') {
                 steps {
