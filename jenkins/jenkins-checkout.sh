a19052bfa0883401b440d43a3ccd5980dae759d5
Brandon Awan <abibangbrandon855@gmail.com>
2023-10-19 06:20:27 +0100

    updates 2023-10-19 06:20:27

---
+++ jenkins/jenkins-checkout.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Replace this with your custom checkout logic
+echo "Running custom checkout script..."
+
+git clone git@github.com:Brandonawan/shared-library.git
+# Add any other custom checkout steps here
+
